5ec416822cae00a8cc34f8b6eef9ecbd0c11a82b
feng1995911 <793396328@qq.com>
2021-09-07 13:21:45 +0800

    #导入BehaviorDesigner

---
+++ Assets/3rd/Behavior Designer/Runtime/Basic Tasks/Network/IsServer.cs
@@ -0,0 +1,13 @@
+using UnityEngine.Networking;
+
+namespace BehaviorDesigner.Runtime.Tasks.Basic.UnityNetwork
+{
+    public class IsServer : Conditional
+    {
+        public override TaskStatus OnUpdate()
+        {
+//            return NetworkServer.active ? TaskStatus.Success : TaskStatus.Failure;
+            return TaskStatus.Failure;
+        }
+    }
+}
